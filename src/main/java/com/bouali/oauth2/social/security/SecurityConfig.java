307a26c3ae3c86df573c70302480865493fa59b7
osparking <jbpark03@gmail.com>
2023-07-11 11:35:16 +0900

    enable oAuth 2 security check

--- src/main/java/com/bouali/oauth2/social/security/SecurityConfig.java
+++ src/main/java/com/bouali/oauth2/social/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.bouali.oauth2.social.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,8 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http)
-			throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(
+						(authorize) -> authorize.anyRequest().authenticated())
+				.oauth2Login(Customizer.withDefaults());
+
 		return http.build();
 	}
 }
