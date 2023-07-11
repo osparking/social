d5556e78e4457d7d71e5fcc63768dd0d9a6ac686
osparking <jbpark03@gmail.com>
2023-07-11 10:38:00 +0900

    security class template created

---
+++ src/main/java/com/bouali/oauth2/social/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.bouali.oauth2.social.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http)
+			throws Exception {
+		return http.build();
+	}
+}
